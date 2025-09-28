56fa1a8138b9086eddb6d7de5aabb9678f34a803
Nikhil Paranjape <indianpoptart@users.noreply.github.com>
2025-09-28 14:36:12 -0400

    Updated chohan np
    - Added changes required by assignment

--- module-3/chohan_np.py
+++ module-3/chohan_np.py
@@ -1,6 +1,7 @@
 """Cho-Han, by Al Sweigart al@inventwithpython.com
 The traditional Japanese dice game of even-odd.
 View this code athttps://nostarch.com/big-book-small-python-projects
+MODIFIED BY Nikhil Paranjape
 Tags: short, beginner, game"""
 
 import random, sys
@@ -8,11 +9,12 @@ import random, sys
 JAPANESE_NUMBERS = {1: 'ICHI', 2: 'NI', 3: 'SAN',
                     4: 'SHI', 5: 'GO', 6: 'ROKU'}
 
-print('''Cho-Han, by Al Sweigart al@inventwithpython.com
+print('''Cho-Han, by Al Sweigart al@inventwithpython.com; Nikhil Paranjape Week 3 
 
 In this traditional Japanese dice game, two dice are rolled in a bamboo
 cup by the dealer sitting on the floor. The player must guess if the
 dice total to an even (cho) or odd (han) number.
+Should you get a 2 or a 7 on a dice roll, you will receive a 10 mon bonus!
 ''')
 
 purse = 5000
@@ -20,7 +22,7 @@ while True:  # Main game loop.
     # Place your bet:
     print('You have', purse, 'mon. How much do you bet? (or QUIT)')
     while True:
-        pot = input('> ')
+        pot = input('np: ') # Made change to input prompt
         if pot.upper() == 'QUIT':
             print('Thanks for playing!')
             sys.exit()
@@ -37,6 +39,8 @@ while True:  # Main game loop.
     dice1 = random.randint(1, 6)
     dice2 = random.randint(1, 6)
 
+    dice_total = dice1 + dice2 # Sum the dice total for bonus usage later
+
     print('The dealer swirls the cup and you hear the rattle of dice.')
     print('The dealer slams the cup on the floor, still covering the')
     print('dice and asks for your bet.')
@@ -45,7 +49,7 @@ while True:  # Main game loop.
 
     # Let the player bet cho or han:
     while True:
-        bet = input('> ').upper()
+        bet = input('np: ').upper() # Changed input prompt here as well
         if bet != 'CHO' and bet != 'HAN':
             print('Please enter either "CHO" or "HAN".')
             continue
@@ -66,18 +70,25 @@ while True:  # Main game loop.
 
     playerWon = bet == correctBet
 
+    # Check for bonus
+    if dice_total == 2 or dice_total == 7: # Check if the player got a 2 or 7 for the bonus
+        purse += 10  # Add 10 mon bonus
+        print(f"Congratulations! You rolled a {dice_total} and got a 10 mon bonus!")
+
+
     # Display the bet results:
     if playerWon:
         print('You won! You take', pot, 'mon.')
         purse = purse + pot  # Add the pot from player's purse.
-        print('The house collects a', pot // 10, 'mon fee.')
-        purse = purse - (pot // 10)  # The house fee is 10%.
+        print('The house collects a', pot * 0.12, 'mon fee.') # Changed to a 12% fee - This notifies the player
+        purse = purse - (pot * 0.12)  # The house fee was 10%, it is now 12%.
     else:
         purse = purse - pot  # Subtract the pot from player's purse.
         print('You lost!')
 
     # Check if the player has run out of money:
-    if purse == 0:
-        print('You have run out of money!')
+    # TODO Fix floating point usage above? Otherwise below will suffice.
+    if purse < 1: # Fix issue for decimal issue less than 1 mon. Decmials do not work above
+        print('You have run out of sufficient money!')
         print('Thanks for playing!')
         sys.exit()
